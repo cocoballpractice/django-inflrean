566a921e940725a95944290d606be3a65b0e05ba
mrcocoball <gunmaru93@gmail.com>
2023-03-06 15:52:59 +0900

    04 - 리스트 뷰 생성

--- practice_01/onememos/urls.py
+++ practice_01/onememos/urls.py
@@ -11,7 +11,7 @@ from . import views
 
 urlpatterns = [
     #path('', views.index, name='index'), #views.py의 함수 매핑. DispatcherServlet -> Controller 확인 및 해당 컨트롤러의 핸들러 메서드로 매핑해주는 느낌
-    path('', views.main),
+    path('', views.main, name='main'),
     path('createMemo/', views.createMemo),
-    path('detail', views.detail, name='detail') # 앞에 /를 안 붙여도 됨 (자동으로 붙음)
+    path('detail', views.detail, name='detail'), # 앞에 /를 안 붙여도 됨 (자동으로 붙음)
 ]
--- practice_01/onememos/views.py
+++ practice_01/onememos/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
+from onememos.models import Memo
 
 # Create your views here.
 
 def main(request): # 기본적인 요청 흐름 url 요청 -> urls.py -> main()
-    #return HttpResponse("안녕 my 월드야")
-    return render(request, 'main.html') # 템플릿 파일명 (앱 폴더 하위 templates에 있다면 경로 지정 하지 않아도 됨)
+
+    lists = Memo.objects.all() # Memo 객체 전부 조회
+    data = {'lists' : lists} # 반드시 딕셔너리 형태로 저장하여 템플릿 파일로 전달해야 함 (map.addAttribute() 느낌)
+
+    return render(request, 'main.html', data) # 템플릿 파일명 (앱 폴더 하위 templates에 있다면 경로 지정 하지 않아도 됨)
 
 def detail(request):
     return HttpResponse("안녕 my detail 월드야")
@@ -16,4 +23,14 @@ def createMemo(request):
 
     memoContent = request.POST['memoContent']
 
-    return HttpResponse("create memo = " + memoContent)
+    # DB 입력
+    article = Memo(description = memoContent)
+    article.save() # 별도의 DAO 없이 바로 가능하구나....
+
+    # 리다이렉트를 위한 HttpResponseRedirect, reverse
+    # redirect() - 특정 url로 리다이렉션
+    # reverse() - URL을 역으로 계산하여 path가 변경되어도 url을 외울 필요가 없음, urls.py에서 만든 URL Patterns의 name을 이용, name을 통해 해당 name의 url을 반환
+    # name 정보 틀릴 경우 페이지 에러 발생
+
+    return HttpResponseRedirect(reverse('main'))
+
